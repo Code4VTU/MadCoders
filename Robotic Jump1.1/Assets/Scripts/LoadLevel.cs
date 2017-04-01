e135bf34aa149e34d2836f7c4cf3ffd94e8e953a
kjechev <daily.invictus@gmail.com>
2017-04-01 13:32:59 +0300

    Robotic Jump 1.1 Remastered

---
+++ Robotic Jump1.1/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class LoadLevel : MonoBehaviour
+{
+
+    public void LevelMenu()
+    {
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            LevelMenu();
+        }
+
+        if (other.gameObject.transform.parent)
+        {
+            Destroy(other.gameObject.transform.parent.gameObject);
+        }
+        else
+        {
+            Destroy(other.gameObject);
+        }
+
+    }
+}
